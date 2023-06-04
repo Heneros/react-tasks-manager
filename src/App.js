7a86273cdef50950fe55b9e621588071b8f3a20b
Heneros <punk60826@gmail.com>
2023-06-04 23:50:27 +0300

    Tasks.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,33 @@
 import React from "react";
 import Form from "./components/Form";
 import { Container } from "@mui/material";
+import { useTasks } from "./contexts/TasksContext";
 import Header from "./components/Header";
+import Tasks from "./components/Tasks";
+
+
 
 function App() {
+  const { tasks } = useTasks();
   return (
     <>
       <Header />
       <Container>
         <Form />
+        <div className="tasks"
+          style={{ displat: "flex", flex: "1", justifyContent: "center" }}
+        >
+
+          {tasks.map(task => (
+            <Tasks
+              key={task.id}
+              name={task.name}
+              description={task.description}
+            />
+          ))}
+
+        </div>
+
       </Container>
     </>
   );
---
+++ src/components/Tasks.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Tasks({ name, description, }) {
+    return (
+        <div className='item-task' style={{ display: "flex", flexDirection: "column", justifyContent: "center" }}>
+            <div className="text" style={{ display: "flex", flexDirection: "row", justifyContent: "space-between" }}>
+                <h5>{name} </h5>
+                <p>{description} </p>
+            </div>
+        </div>
+    )
+}
