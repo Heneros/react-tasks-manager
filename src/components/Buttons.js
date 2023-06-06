58f82ad70bddd6bc609ef30faf57add097e96145
Heneros <punk60826@gmail.com>
2023-06-07 00:12:53 +0300

    Buttons.js

--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -1,9 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useTasks } from '../contexts/TasksContext'
 
 export default function Buttons() {
     const { tasks } = useTasks();
+    const [filter, setFilter] = useState('all');
 
+    const filteredTasks = tasks.filter(task => {
+        if (filter === 'all') {
+            return true;
+        }
+    })
     return (
         <div></div>
     )
