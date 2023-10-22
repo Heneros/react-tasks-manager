109db526654da802c30f2307be0c9c4fd465c80e
Heneros <punk60826@gmail.com>
2023-10-22 10:44:17 +0300

    Updates

---
+++ precache-manifest.bd8ddbfaa53b53177a90abac0cba9bcf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "00474ee2750c077ec729",
+    "url": "/react-tasks-manager/static/css/main.d008ec4a.chunk.css"
+  },
+  {
+    "revision": "00474ee2750c077ec729",
+    "url": "/react-tasks-manager/static/js/main.00474ee2.chunk.js"
+  },
+  {
+    "revision": "d99c4d3408f31a58da47",
+    "url": "/react-tasks-manager/static/js/1.d99c4d34.chunk.js"
+  },
+  {
+    "revision": "9e395b53b33e9e7b3081",
+    "url": "/react-tasks-manager/static/js/runtime~main.9e395b53.js"
+  },
+  {
+    "revision": "27758a7f5113a03446b9f11ebad05e77",
+    "url": "/react-tasks-manager/index.html"
+  }
+];
